7b1e268cec7e9435f46a69204441fca6f93701ab
Dinesh Prasad <88045947+Dinesh9928@users.noreply.github.com>
2024-12-04 10:42:24 +0530

    Time: 298 ms (95.46%), Space: 67 MB (92.93%) - LeetHub

--- 2880-select-data/2880-select-data.py
+++ 2880-select-data/2880-select-data.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
 def selectData(students: pd.DataFrame) -> pd.DataFrame:
-    return students.loc[students['student_id'] == 101, ['name', 'age']]
+    # return students.loc[students['student_id'] == 101, ['name', 'age']]
+    # return students.loc[students["student_id"] == 101, "name" :]
+    # return students[students.student_id==101][['name','age']]
+    df = students[students['student_id'] == 101]
+    return df[['name','age']]
     
