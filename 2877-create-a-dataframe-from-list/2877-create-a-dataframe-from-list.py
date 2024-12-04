d9d0985f9c439fc3f5bdfbb2b8c5a6e86b6cedfe
Dinesh Prasad <88045947+Dinesh9928@users.noreply.github.com>
2024-12-04 10:18:06 +0530

    Time: 341 ms (59.95%), Space: 65.4 MB (97.67%) - LeetHub

--- 2877-create-a-dataframe-from-list/2877-create-a-dataframe-from-list.py
+++ 2877-create-a-dataframe-from-list/2877-create-a-dataframe-from-list.py
@@ -1,3 +1,13 @@
+# import pandas as pd
+
+# def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
+#     return pd.DataFrame(
+#     {
+#         "student_id": [item[0] for item in student_data],
+#         "age": [item[1] for item in student_data]
+#     }
+#     )
+
 import pandas as pd
 
 def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
