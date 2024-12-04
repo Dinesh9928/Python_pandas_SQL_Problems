805b9560c56e1ccc863bdc3aa04c3ac196d9bdac
Dinesh Prasad <88045947+Dinesh9928@users.noreply.github.com>
2024-12-04 10:32:08 +0530

    Time: 355 ms (64.82%), Space: 66 MB (95.71%) - LeetHub

--- 2879-display-the-first-three-rows/2879-display-the-first-three-rows.py
+++ 2879-display-the-first-three-rows/2879-display-the-first-three-rows.py
@@ -1,5 +1,20 @@
+
+
 import pandas as pd
 
 def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
-    return employees.head(3)
-    
+    return employees.loc[0:2]
+
+
+#      import pandas as pd
+#      def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
+#         return employees.head(3)
+
+#      def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
+#         return employees.loc[0:2]
+
+#      def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
+#         return employees[:3]
+ 
+
+
