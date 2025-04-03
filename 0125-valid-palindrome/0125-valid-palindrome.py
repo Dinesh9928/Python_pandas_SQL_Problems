cba21a6be917a5846e5f0df42235f819e1543438
Dinesh Prasad <88045947+Dinesh9928@users.noreply.github.com>
2025-04-03 21:12:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,21 +1,19 @@
 class Solution:
-    def checkalphanumric(self, ch: str)-> bool:
-        return 'a' <= ch <= 'z' or '0'<= ch <= '9'
-
-    def isPalindrome(self, s: str) -> bool:
+    def isPalindrome(self, s: str) -> bool:        
         s = s.lower()
-        i , j = 0 , len(s)-1
+        res = ""
+        for i in s:
+            if 'a' <= i <= 'z' or '0'<= i <= '9':
+                res += i
 
+        print(res)
+    
+        i, j = 0, len(s) - 1
         while i < j:
-            while( i < j and not self.checkalphanumric(s[i])):
-                i += 1
-            while(i < j) and not self.checkalphanumric(s[j]):
-                j -=1
-
-            if(s[i] != s[j]):
+            if i < j and s[i] != s[j]:  # Ensure proper comparison
                 return False
-            i +=1
-            j -=1
+            i += 1
+            j -= 1
 
         return True
             
