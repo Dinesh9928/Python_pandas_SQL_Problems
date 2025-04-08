989f040e7bebf3a21a75c7b9b85e083374f0d5e2
Dinesh Prasad <88045947+Dinesh9928@users.noreply.github.com>
2025-04-08 12:06:01 +0530

    Time: 18 ms (90.11%), Space: 23.2 MB (79.47%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,29 +1,17 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prefix = [1]* len(nums)
-        postfix = [1]* len(nums)
-
-        # prefix values [1, 1, 1, 1], num = [1,2,3,4]
-
-        for i in range(1, len(nums)):
-            prefix[i] =nums[i-1]* prefix[i-1] # pre = [1, 1, 2, 6]
-
         
+        res = [1]*len(nums)
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] =prefix
+            prefix *=nums[i] # res=[1,1,2,6], num = [1,2,3,4]
 
-        # postfix Vales [1, 1, 1, 1], num = [1, 2, 3, 4]
-        for i in range(len(nums)-2, -1, -1):
-            postfix[i] = nums[i+1] * postfix[i+1] # post = [24,12,4,1]
-        
-        print(postfix)
-        print(prefix)
-            
-        # storing the values 
-        res =[]
-        for i in range(0, len(nums)):
-            res.append(prefix[i]*postfix[i])
+        postfix = 1
+        for i in range(len(nums)-1, -1, -1):
+            res[i] *=postfix
+            postfix *= nums[i] #res =[24,12,8,6]]
+
         return res
 
-        # nums = [1,2,3,4]
-        # prefix = [1,1, 2, 6]
-        # postfix = [24,12,4,1]
-        # res = pre * post = [24,12,8,6]
+       
